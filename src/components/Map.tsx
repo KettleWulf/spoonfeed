75578254c743cae35d239d8a33de4af06156ece2
Philip <m.p.o.andersson@gmail.com>
2025-08-26 18:17:45 +0200

    added parts of map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,22 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const mapApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const containerStyle = {
+    width: "400px",
+    height: "400px",
+}
+
+const center = {
+    lat: -3.745,
+    lng: -38.523,
+}
+const Map = () => {
+  return (
+    <LoadScript
+        googleMapsApiKey={mapApiKey}>
+    
+    </LoadScript>
+  )
+}
+
+export default Map
