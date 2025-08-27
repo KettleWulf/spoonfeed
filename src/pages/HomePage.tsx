4f928313829f0b91e513a21be17afd36b4c5c884
Olle Wistedt <wistedt@live.com>
2025-08-27 14:54:06 +0200

    Centered content on HomePage bc I couldn't help myself

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,19 @@
 
+import { Col, Container, Row } from "react-bootstrap";
 import EstablishmentFormModal from "../components/EstablishmentFormModal";
 import Map from "../components/Map";
 
 
 const HomePage = () => {
 	return (
-	<>
-		<h1>HomePage</h1>
-		<Map />
-		<EstablishmentFormModal onSave={() => {return}} isAdmin={true}/>
-	</>
+		<Container className="py-2">
+			<Row className="justify-content-center">
+				<Col md={8} lg={6}>
+					<Map />
+					<EstablishmentFormModal onSave={() => {}} isAdmin={true}/>
+				</Col>
+			</Row>
+</Container>
 	)
 }
 
