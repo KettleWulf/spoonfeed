e4f742be00427c5f5cd9dbaac68b5a1bf8f463bf
Philip <m.p.o.andersson@gmail.com>
2025-08-27 10:44:28 +0200

    fixed merge conflicts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,17 @@
-<<<<<<< HEAD
+
+import EstablishmentFormModal from "../components/EstablishmentFormModal";
 import Map from "../components/Map";
 
 
 const HomePage = () => {
-	return <>
+	return (
+	<>
 		<h1>HomePage</h1>
 		<Map />
-	</>;
-=======
-import EstablishmentFormModal from "../components/EstablishmentFormModal";
+		<EstablishmentFormModal onSave={() => {return}} isAdmin={true}/>
+	</>
+	)
+}
 
-
-const HomePage = () => {
-	return <EstablishmentFormModal onSave={() => {return}} isAdmin={true}/>;
->>>>>>> 9b509d4c51fa5831ab4533d8dc530bb1ce4e9119
-};
 
 export default HomePage;
