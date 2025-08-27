2594a73febf6e208791189e6064cb733f505207b
Lucas-Hernandez99 <lucashernandez1999@outlook.com>
2025-08-27 16:20:32 +0200

    added function so you can logout

--- src/pages/partials/Navigation.tsx
+++ src/pages/partials/Navigation.tsx
@@ -1,20 +1,39 @@
 import { useContext } from "react"
-import { Container } from "react-bootstrap"
+import { Container, NavDropdown } from "react-bootstrap"
 import { Nav } from "react-bootstrap"
 import { Navbar } from "react-bootstrap"
 import { AuthContext } from "../../context/AuthContext"
+import { FirebaseError } from "firebase/app"
+import { toast } from "react-toastify"
 
 
 const Navigation = () => {
 
-     const authContext = useContext(AuthContext)
-    
-        if (!authContext) {
-            throw new Error("Trying to use authcontext outside of authcontextprovider");
-    
+    const authContext = useContext(AuthContext)
+
+    if (!authContext) {
+        throw new Error("Trying to use authcontext outside of authcontextprovider");
+
+    }
+
+    const { currentUser, logOut } = authContext
+
+    const handleLogOut = async () => {
+
+        try {
+
+           const logout =  await logOut()
+
+           return logout
+
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+                toast.error(e.message)
+            } else if (e instanceof Error) {
+                toast.error(e.message)
+            }
         }
-    
-        const { currentUser } = authContext
+    }
 
     return (
         <>
@@ -25,14 +44,29 @@ const Navigation = () => {
                         <Nav.Link href="*">About us</Nav.Link>
                         <Nav.Link href="*">Resturants</Nav.Link>
                         <Nav.Link href="*">Send us a tip</Nav.Link>
-                        {currentUser 
-                        ? <p>{currentUser.email}</p>
-                        : <Nav.Link href="/login">Log In</Nav.Link>
+                        {currentUser
+
+                            ?
+                            <NavDropdown
+                                id="nav-dropdown-dark-example"
+                                title={currentUser.email}
+                                menuVariant="dark"
+                            >
+
+                                <NavDropdown.Item 
+                                 className="text-danger fw-bold"
+                                 onClick={handleLogOut} href="*"
+                                >
+                                        Log Out
+                                </NavDropdown.Item>
+                            </NavDropdown>
+
+                            : <Nav.Link href="/login">Log In</Nav.Link>
                         }
                     </Nav>
                 </Container>
             </Navbar>
-            
+
         </>
     )
 }
