5e688253f3db4b28bbd28acc1aec544945b40d47
Philip <m.p.o.andersson@gmail.com>
2025-09-02 19:42:43 +0200

    double loadscripts caused the original problem when the map didn't load sometimes.. Removed both and use useJsApiLoader instead

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import AuthContextProvider from "./context/AuthContextProvider.tsx";
-import { LoadScript } from "@react-google-maps/api";
 
-const mapApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<AuthContextProvider>
 			<BrowserRouter>
-				<LoadScript googleMapsApiKey={mapApiKey}>
+
 					<App />
-				</LoadScript>
+
 			</BrowserRouter>
 		</AuthContextProvider>
 	</StrictMode>
