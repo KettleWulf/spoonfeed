31af13ba343e2e0402d6c7c385e54975aed7f38d
Olle Wistedt <wistedt@live.com>
2025-09-02 21:17:02 +0200

    Boostrapified HomePage a bit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,43 +1,68 @@
-
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import Map from "../components/Map";
 import { addDoc, serverTimestamp } from "firebase/firestore";
 import { newPlacesCol } from "../services/Firebase";
 import { toast } from "react-toastify";
 import useAuth from "../hooks/useAuth";
-import type { PlaceFormData } from "../types/Place.types";
-
-
+import type { Place, PlaceFormData } from "../types/Place.types";
+import { useGetPlacesByCity } from "../hooks/useGetPlacesByCity";
+import SortableTable from "../components/SortableTable";
+import type { ColumnDef } from "@tanstack/react-table";
 
 const HomePage = () => {
-
 	const { currentUser } = useAuth();
+	const { data: places } = useGetPlacesByCity("");
+
+	const columnDefs: ColumnDef<Place>[] = [
+		{ accessorKey: "name", header: "Name" },
+		{ accessorKey: "category", header: "Category" },
+	];
 
 	const addPlace = async (place: PlaceFormData) => {
-		// Create document with a generated ID
 		await addDoc(newPlacesCol, {
 			...place,
 			createdAt: serverTimestamp(),
 			updatedAt: serverTimestamp(),
-			isSuggestion: !currentUser
+			isSuggestion: !currentUser,
 		});
-
-
-		// 🥂
 		toast.success("Place added!");
-	}
-
+	};
 
 	return (
-		<Container className="py-2">
-			<Row className="justify-content-center">
-				<Col md={8} lg={6}>
-					<Map onSavePlace={addPlace}/>
+		<Container fluid className="py-3">
+			<Row className="g-4">
+
+				<Col xs={12} lg={{ span: 4, offset: 4 }}>
+					<Card className="shadow-sm rounded-3">
+						<Card.Header as="h5" className="bg-white border-0">
+							Nearby map
+						</Card.Header>
+						<Card.Body className="pt-0">
+							<Map onSavePlace={addPlace} />
+						</Card.Body>
+					</Card>
+				</Col>
+
+
+				<Col xs={12} lg={2} className="me-auto">
+					<Card className="shadow-sm rounded-3 h-100">
+						<Card.Header as="h5" className="bg-white border-0">
+							Places
+						</Card.Header>
+						<Card.Body className="pt-0">
+							{places && (
+								<SortableTable
+									columns={columnDefs}
+									data={places}
+									getRowLink={(row) => `/places/${row._id}`}
+								/>
+							)}
+						</Card.Body>
+					</Card>
 				</Col>
 			</Row>
 		</Container>
-	)
-}
-
+	);
+};
 
 export default HomePage;
