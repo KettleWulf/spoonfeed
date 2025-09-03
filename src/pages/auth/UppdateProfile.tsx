41f9dcaf414f016328647c3b55479cfedb5ea52b
Lucas-Hernandez99 <lucashernandez1999@outlook.com>
2025-09-03 19:42:00 +0200

    added a component for uppdateProfilePage

--- src/pages/auth/UppdateProfile.tsx
+++ src/pages/auth/UppdateProfile.tsx
@@ -1,15 +1,14 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import type { UppdateUserCredentials } from '../../types/User.types';
 import { FirebaseError } from 'firebase/app';
 import { toast } from "react-toastify";
-import { Card, Col, Container, Image, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import useAuth from '../../hooks/useAuth';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../../services/Firebase';
-import blankProfile from "../../assets/images/blank-profile-picture-973460_1280.png"
+import UpdateProfileFrom from '../../components/auth/UpdateProfileFrom';
 
 
 
@@ -19,11 +18,9 @@ import blankProfile from "../../assets/images/blank-profile-picture-973460_1280.
 const UppdateProfile = () => {
     const { currentUser, changeEmail, changePassword, changePhotoUrl, changeUserName, userName, userUrl, userEmail, reloadForm, updateUserDataName, updateUserDataPhoto } = useAuth()
 
-    const { handleSubmit, register, reset, watch, formState: { errors, isSubmitting } } = useForm<UppdateUserCredentials>()
+    const { reset, } = useForm<UppdateUserCredentials>()
 
-    const [urlUpload, seturlUpload] = useState<string | null>(null)
 
-    const password = watch("password")
 
     useEffect(() => {
         reset({
@@ -114,105 +111,13 @@ const UppdateProfile = () => {
                 <Col md={{ span: 6, offset: 3 }}>
                     <Card className="mb-3">
                         <Card.Body>
-                            <Card.Title className="mb-3">Profile</Card.Title>
-
-                            <div>
-                                <Image alt={currentUser?.photoURL ?? blankProfile } src={userUrl || urlUpload || undefined} roundedCircle className="img-cover w-75" />
-                            </div>
-
-
-                            { /* Sign up form */}
-                            <Form onSubmit={handleSubmit(onUppdateProfile)}>
-
-                                {/* Email */}
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email"
-                                        placeholder="Email"
-                                        {...register("email", {
-                                            minLength: {
-                                                value: 3,
-                                                message: "You have to have at least 3 characters long"
-                                            }
-                                        })}
-
-                                    />
-                                    {errors.email
-                                        ? <p className="text-danger">{errors.email.message || "invalid"}</p>
-                                        : <Form.Text className="text-muted">
-                                            We'll never share your email with anyone else.
-                                        </Form.Text>
-                                    }
-                                </Form.Group>
-
-
-                                {/* Name */}
-                                <Form.Group className="mb-3" controlId="displayName">
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control type="text"
-                                        placeholder="Sven"
-                                        {...register("username", {
-                                            minLength: {
-                                                value: 3,
-                                                message: "You have to have at least 3 characters long"
-                                            }
-                                        })}
-                                    />
-                                    {errors.username && <p className="text-danger">{errors.username.message || "invalid"}</p>}
-                                </Form.Group>
-
-
-                                <Form.Group className="mb-3" controlId="photoUrl">
-                                    <Form.Label>Profile Piqture</Form.Label>
-                                    <Form.Control type="file"
-                                        accept='image/png, image/jpeg, image/jpg'
-                                        {...register("photoUrl", {
-                                            onChange: (e) => {
-                                                e.target.files?.[0];
-                                                seturlUpload(userUrl)
-                                            }
-                                        })}
-                                    />
-
-                                    {errors.photoUrl && <p className="text-danger">{errors.photoUrl.message || "invalid"}</p>}
-                                </Form.Group>
-
-
-                                {/* Password */}
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" autoComplete="new-password" placeholder="Password"
-                                        {...register("password", {
-
-                                            minLength: {
-                                                message: "Enter at least 6 characters",
-                                                value: 6
-                                            }
-                                        })}
-                                    />
-                                    {errors.password && <p className="text-danger">{errors.password.message || "invalid"}</p>}
-                                </Form.Group>
-
-
-                                {/* Comfirm Password */}
-                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                    <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control type="password" autoComplete="off" placeholder="Confirm Password"
-                                        {...register("confirmPassword", {
-
-                                            validate: (value) => value === password || "Paswords do not match"
-                                        })}
-                                    />
-                                    {errors.confirmPassword && <p className="text-danger">{errors.confirmPassword.message || "invalid"}</p>}
-                                </Form.Group>
-
-
-                                <Button variant="primary" type="submit" disabled={isSubmitting}>
-                                    Save
-                                </Button>
-
-                            </Form>
-
+                            {<UpdateProfileFrom 
+                            onUppdateProfile={onUppdateProfile}
+                            currentUser={currentUser}
+                            userUrl={userUrl}
+                            userEmail={userEmail}
+                            userName={userName}
+                            />}
                         </Card.Body>
                     </Card>
 
